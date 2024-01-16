974f03eebda9a2ccb12eed16ec05455ec2cd661e
Maridi <mmaridi@uab.edu>
2024-01-15 18:39:37 -0600

    first commit

---
+++ lab1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*
+    Name : Misritha Maridi
+    Blazer ID : mmaridi
+    University : UAB
+    Department : CS
+*/
+
+int main() { 
+    // printf("Hello World\n");
+    int given_number;
+    scanf("%d",&given_number);
+    if (given_number%2==0){
+        printf("The number is an even  number\n");
+    } else {
+        printf("The number is an odd number\n");
+    }
+    return 0;
+}
